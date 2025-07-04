f3143f3253905fa090eaf5d8fdd802f95e897491
Juan José López <j.lopez16@outlook.com>
2025-07-04 08:22:17 -0600

    refactor: improve configuration management and data types
    - Move database configuration from SnowflakeConfig to application.yml
    - Update CustomerDto and CustomerDocument to use String for customerId
    - Remove hardcoded database credentials from code
    - Move sensitive configuration to environment-specific files

--- customer-api/src/main/java/org/recluit/customerapi/config/SnowflakeConfig.java
+++ customer-api/src/main/java/org/recluit/customerapi/config/SnowflakeConfig.java
@@ -11,16 +11,6 @@ import javax.sql.DataSource;
 public class SnowflakeConfig {
 
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("net.snowflake.client.jdbc.SnowflakeDriver");
-        ds.setUrl("jdbc:snowflake://BSTKSKS-UV61897.snowflakecomputing.com/?db=SNOWFLAKE_SAMPLE_DATA&schema=TPCDS_SF10TCL");
-        ds.setUsername("rarezas");
-        ds.setPassword("fKs222bshM8Jr-wfUCHp");
-        return ds;
-    }
-
-    @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
